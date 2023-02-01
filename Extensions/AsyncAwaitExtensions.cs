0bde0347641743c179573419ae7948dd9a6099cc
Christopher Richmond <lancer1977@gmail.com>
2023-02-01 17:35:13 -0500

    async ext

--- Extensions/AsyncAwaitExtensions.cs
+++ Extensions/AsyncAwaitExtensions.cs
@@ -7,7 +7,7 @@ namespace PolyhydraGames.Extensions
 { 
     public static class AsyncTaskExtensions
     {
-        public static async Task<IList<T>> ToListAsync<T>(this Func<Task<IEnumerable<T>>> source)
+        public static async Task<List<T>> ToListAsync<T>(this Func<Task<IEnumerable<T>>> source)
         {
             var enumerable = await source.Invoke();
             return enumerable.ToList();
@@ -18,12 +18,27 @@ namespace PolyhydraGames.Extensions
             var enumerable = await source;
             return enumerable.ToList();
         }
-        public static async Task<List<T>> OrderByAsync<T>(this Task<IEnumerable<T>> source, Func<T, object> orderer)
+  
+        public static async Task<IEnumerable<T>> OrderByAsync<T>(this Task<IEnumerable<T>> source, Func<T, object> orderer)
         {
             var enumerable = await source;
-            return enumerable.OrderBy(orderer).ToList();
+            return enumerable.OrderBy(orderer);
         }
-        
+
+        public static async Task<IEnumerable<TResult>> SelectAsync<T, TResult>(this Task<IEnumerable<T>> source, Func<T, TResult> selector)
+        {
+            var enumerable = await source;
+            return enumerable.Select(selector);
+        }
+
+        public static async Task<bool> AnyAsync<T>(this Task<IEnumerable<T>> source)
+        {
+            var enumerable = await source;
+            return enumerable.Any();
+        }
+
+
+
         public static async Task ExecuteAsync(this Func<Task> act, int times = 1)
         {
             if (times <= 0)
