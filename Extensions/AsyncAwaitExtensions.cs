17e87b0aa6318c1199c5a2b8f55bf7ed77eb5c35
Christopher Richmond <lancer1977@gmail.com>
2023-02-01 01:10:51 -0500

    no message

--- Extensions/AsyncAwaitExtensions.cs
+++ Extensions/AsyncAwaitExtensions.cs
@@ -1,12 +1,35 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace PolyhydraGames.Extensions
-{
-    public static class AsyncAwaitExtensions
+{ 
+    public static class AsyncTaskExtensions
     {
-        public static void Await(this Task task)
+        public static async Task<IList<T>> ToListAsync<T>(this Func<Task<IEnumerable<T>>> source)
         {
-            task.GetAwaiter().GetResult();
+            var enumerable = await source.Invoke();
+            return enumerable.ToList();
+        }
+
+        public static async Task<List<T>> ToListAsync<T>(this Task<IEnumerable<T>> source)
+        {
+            var enumerable = await source;
+            return enumerable.ToList();
+        }
+        public static async Task<List<T>> OrderByAsync<T>(this Task<IEnumerable<T>> source, Func<T, object> orderer)
+        {
+            var enumerable = await source;
+            return enumerable.OrderBy(orderer).ToList();
+        }
+        
+        public static async Task ExecuteAsync(this Func<Task> act, int times = 1)
+        {
+            if (times <= 0)
+                return;
+            for (var index = 0; index < times; ++index)
+                await act();
         }
     }
 }
