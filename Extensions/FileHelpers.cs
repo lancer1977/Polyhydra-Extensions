3c0f9a5742344b712cad4d32d5774dace396a0b8
Christopher Richmond <lancer1977@gmail.com>
2024-01-21 14:34:48 -0500

    FileExtensions

---
+++ Extensions/FileHelpers.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO; 
+
+namespace PolyhydraGames.Extensions
+{
+    public static class FileExtensionHelpers
+    {
+        static string[] audioExtensions = { ".mp3", ".wav", ".ogg", ".flac", ".aac", ".wma" /* Add more as needed */ };
+        static string[] imageExtensions = { ".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff" /* Add more as needed */ };
+        public static bool IsImageFile(this string file)
+        {
+            var extension = Path.GetExtension(file);
+            return Array.Exists(imageExtensions, ext => ext == extension);
+        }
+        public static bool IsAudioFile(this string file)
+        {
+            var extension = Path.GetExtension(file);
+            return Array.Exists(audioExtensions, ext => ext == extension);
+        }
+        public static bool MatchesPattern(string filename, string pattern)
+        {
+            return Path.GetFileName(filename).Contains(pattern);
+        }
+
+
+    }
+}
