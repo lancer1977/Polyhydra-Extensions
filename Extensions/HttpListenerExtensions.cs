56f4a2a8eb7c7526ce9478eb151446c0468384f2
Christopher Richmond <lancer1977@gmail.com>
2026-03-17 05:06:25 -0400

    Code cleanup: modernize C# syntax, fix issues
    - CryptoExtensions: Use modern using statements, remove trailing semicolon, simplify
    - HttpListenerExtensions: Fix typo 'AddCorsheaders' -> 'AddCorsHeaders', use 'using System.Text', correct content type to 'application/json'
    - JsonSegmentExtensions: Remove unused 'using System.Diagnostics', remove Debug.WriteLine, add nullable return type
    - StreamHelper: Use 'leaveOpen: true' to avoid disposing the stream we're returning

--- Extensions/HttpListenerExtensions.cs
+++ Extensions/HttpListenerExtensions.cs
@@ -1,41 +1,35 @@
 using System.Net;
+using System.Text;
 
 namespace PolyhydraGames.Extensions;
 
 public static class HttpListenerExtensions
 {
-    public static void WriteJson(this HttpListenerContext context, string html)
+    public static void WriteJson(this HttpListenerContext context, string json)
     {
-        context.AddCorsheaders();
-        var buffer = System.Text.Encoding.UTF8.GetBytes(html);
-        context.Response.ContentType = "text/json";
+        context.AddCorsHeaders();
+        var buffer = Encoding.UTF8.GetBytes(json);
+        context.Response.ContentType = "application/json";
         context.Response.ContentLength64 = buffer.Length;
-        // Write the main response content
         context.Response.OutputStream.Write(buffer, 0, buffer.Length);
-        // Close the response
         context.Response.Close();
     }
 
     public static void WriteHtml(this HttpListenerContext context, string html)
     {
-        context.AddCorsheaders();
-        var buffer = System.Text.Encoding.UTF8.GetBytes(html);
+        context.AddCorsHeaders();
+        var buffer = Encoding.UTF8.GetBytes(html);
         context.Response.ContentType = "text/html";
         context.Response.ContentLength64 = buffer.Length;
-
-        // Write the main response content
         context.Response.OutputStream.Write(buffer, 0, buffer.Length);
-
-        // Close the response
         context.Response.Close();
     }
 
-    public static void AddCorsheaders(this HttpListenerContext context)
+    public static void AddCorsHeaders(this HttpListenerContext context)
     {
         context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
         context.Response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
         context.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type");
         context.Response.Headers.Add("X-Content-Type-Options", "nosniff");
-
     }
 }
--- Extensions/StreamHelper.cs
+++ Extensions/StreamHelper.cs
@@ -7,7 +7,7 @@ public static class StreamHelper
     public static Stream ToStream(this string str)
     {
         var stream = new MemoryStream();
-        var writer = new StreamWriter(stream);
+        using var writer = new StreamWriter(stream, leaveOpen: true);
         writer.Write(str);
         writer.Flush();
         stream.Position = 0;
